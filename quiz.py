ed5989d33ef764d6569e37274200c23fd6d7bb1a
stanislava-1 <stanislava-1@users.noreply.github.com>
2017-01-23 15:55:37 +0100

    Minor improvements

--- quiz.py
+++ quiz.py
@@ -1,13 +1,14 @@
 
-#This is the first text displayed on the screen after running a program and it explains player how to choose a level:
+# The first text displayed on the screen after running a program and it explains player how to choose a level:
 first_text = '''
 Choose a level of the game:
 1 - easy
 2 - medium
 3 - hard
+4 - end game
 
 '''
-#the text for level 1 (easy):
+# The text for level 1 (easy):
 level_1 = '''
 Level 1: Who is who in fairytales?
 
@@ -23,10 +24,10 @@ ___4___ had very long golden hair and she was shut in a tower with neither stair
 _____________________________________________________________________________________________
 
 '''
-#the list of correct answers for the level 1:
+# The list of correct answers for the level 1:
 answers_1 = ['Cinderella', 'Snow White', 'Sleeping Beauty', 'Rapunzel']
 
-#the text for level 2 (medium):
+# The text for level 2 (medium):
 level_2 = '''
 Level 2: Capital cities
 
@@ -44,10 +45,10 @@ Moscow is the capital of ___5___.
 ___________________________________________________
 
 '''
-#the list of correct answers for the level 2:
+# The list of correct answers for the level 2:
 answers_2 = ['Paris', 'Turkey', 'Vienna', 'London', 'Russia']
 
-#the text for level 3 (hard):
+# The text for level 3 (hard):
 level_3 = '''
 Level 3:  Intro to programming
 
@@ -69,45 +70,74 @@ as the test expression is True.
 ________________________________________________________________________
 
 '''
-#the list of correct answers for the level 2:
+# The list of correct answers for the level 3:
 answers_3 = ['Language', 'Value', 'String', 'Outputs', 'True', 'While']
 
-#to make it easy to choose the right level text in a later code, I made a list of level texts:
+# A list of level texts:
 list_of_levels = [level_1, level_2, level_3]
-#to make it easy to choose the relevant answers in a later code, I made a list of answer lists:
+# A list of answer lists:
 lists_of_answers = [answers_1, answers_2, answers_3]
 
-#this function takes an integer as input and returnes the relevant level text. The integer represents the game level and the player enters it at the begining of the game:
+# This function takes an integer as input and returnes the relevant level text. 
+# The integer represents the game level and the player enters it at the begining of the game:
 def choose_level_text(level_number):
 	return list_of_levels[int(level_number) - 1]
 
-#this function takes an integer as input and returnes the list of answers that belong to the level that is represented by the integer (it is the same number as in the previous function):
+# This function takes an integer as input and returnes the list of answers 
+# that belong to the level that is represented by the integer 
+# (it is the same number as in the previous function):
 def choose_answers(level_number):
 	return lists_of_answers[int(level_number) - 1]
 
-#this function plays one level of the game. The code is universal for all three levels. 
-#It takes as inputs a string level_text (the text that belongs to the level) and a list called answers (the answers that belong to the level):
+# This function plays one level of the game. The code is universal for all three levels. 
+# It takes as inputs a string level_text (the text that belongs to the level) 
+# and a list of answers that belong to the level:
 def play_level(level_text, answers):
-	print level_text #prints the level text so that the player could see the task
-	number = 1 #this variable represents the number of a blank space
+	# print the level text so that the player could see the task
+	print level_text
+	# this variable represents the number of a blank space 
+	number = 1 
 	while number <= len(answers):
-		user_input = raw_input("What should go in blank number " + str(number) + "? ") #the player is prompted to enter the answer
-		if user_input.title() == answers[number - 1]: #it evaluates whether his answer is correct. 
-			to_replace = '___' + str(number) + '___' # choosing the relevant blank space
-			level_text = level_text.replace(to_replace, answers[number - 1]) #the blank space is replaced with the correct answer
-			print level_text #the player is shown how it looks replaced
-			number = number + 1 #and we add 1 to the variable number so that the player could be prompted to guess following word
-		else:
+		#the player is asked to enter the answer
+		user_input = raw_input("What should go in blank number " + str(number) + "? ")
+		# evaluate whether his answer is correct.  
+		if user_input.title() == answers[number - 1]:
+			# choose the relevant blank space 
+			to_replace = '___' + str(number) + '___'
+			# the blank space is replaced with the correct answer 
+			level_text = level_text.replace(to_replace, answers[number - 1])
+			# the player is shown how it looks replaced 
+			print level_text
+			# and we add 1 to the variable number so that the player could be 
+			# asked to guess following word
+			number = number + 1 
+		else: 
 			print 'Try again!'
-	return 'CONGRATULATIONS!!! You mastered this level!'
 
-#this function plays the whole game. It has no inputs and it returns the function that runs the choosen level.
+	print 'CONGRATULATIONS!!! You mastered this level!'
+	play_game()
+
+
+# This function plays the whole game. It has no inputs and it returns the function 
+# that runs the choosen level.
 def play_game():
-	print first_text #It prints the introduction text firstly
-	user_input = raw_input("Enter number: " ) #it prompts the player to enter a number representing the game level
-	level_text = choose_level_text(user_input) #it calls the function for choosing the right level text
-	answers = choose_answers(user_input) #it calls the function for choosing the right list of answers
-	return play_level(level_text, answers) #and it returns the code that runs the choosen level
+	# print the introduction text firstly
+	print first_text
+	# ask the player to enter a number representing the game level 
+	user_input = raw_input("Enter number: " )
+	if int(user_input) < 4:
+		# call the function for choosing the right level text 
+		level_text = choose_level_text(user_input)
+		# call the function for choosing the right list of answers 
+		answers = choose_answers(user_input)
+		# return the code that runs the choosen level
+		return play_level(level_text, answers)
+	else:
+		return end_game()
 
-print play_game()
+def end_game():
+	print 'You have quit the game. Run the module again, if you want to play again.'
+
+
+play_game()
 
